f5a3f723a258d64e63297e7badc3cc6997173343
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-01-23 15:53:10 +0100

    issues

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,15 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
+import CreatePost from "./pages/CreatePost";
+import Login from "./pages/Login";
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/createpost" element={<CreatePost />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );

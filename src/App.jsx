242888b4c7a9bcb57bb4bfd971e72ae957fa0753
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-01-23 15:05:45 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// export default App
+import React, { useState } from "react";
+
+import "./App.css";
+const App = () => {
+  return (
+    <>
+      <h1>Hello world</h1>
+    </>
+  );
+};
+export default App;
